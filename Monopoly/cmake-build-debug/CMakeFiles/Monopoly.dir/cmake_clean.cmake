b233a9191eae115c9b2a0b4b9494baeeca3358ea
alev30 <35115285+alev30@users.noreply.github.com>
2018-05-22 12:56:39 -0700

    Add files via upload

---
+++ Monopoly/cmake-build-debug/CMakeFiles/Monopoly.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Monopoly.dir/main.cpp.o"
+  "Monopoly.pdb"
+  "Monopoly"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Monopoly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Monopoly/print_statements.c
@@ -0,0 +1,34 @@
+//
+// Created by mfbut on 1/11/2018.
+//
+
+printf("How many players will be playing: ");
+
+printf("The winners are\n");
+printf("Player %d\n"
+
+printf("Player %d please choose an action\n"
+printf("1: End Turn\n");
+printf("1: Roll Dice\n");
+
+printf("2: Inspect Player\n");
+printf("3: Leave Game\n");
+printf("Your action: ");
+
+printf("You rolled a %d!\n"
+
+printf("Which player would you like to inspect?\n");
+printf("%d for Player %d\n"
+printf("Your choice: ");
+
+printf("Player %d\n"
+printf("  Cash: $%d\n"
+printf("  Properties owned\n");
+
+printf("Player %d would you like to buy %s for $%d?\n"
+"The rent on this property is $%d\n"
+"You currently have $%d\n"
+"Y for yes, N for no: ",
+
+printf("Player %d went bankrupt to Player %d\n"
+printf("Player %d payed Player %d $%d for landing on %s\n"
